5e13549bf127e0a156b48c3bd9ef6f3b401acbe9
Logeshwaran-N <logeshwaran082@gmail.com>
2024-10-31 12:24:31 +0530

    updated env

--- main.go
+++ main.go
@@ -1,21 +1,14 @@
 package main
 
 import (
-	"log"
 	"tasks-service/handlers"
 
 	"github.com/gin-contrib/cors"
 
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	// Load environment variables
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 
 	// Initialize MongoDB connection
 	handlers.ConnectDB()
